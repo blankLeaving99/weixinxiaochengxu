dc3d7a907584c16b91dad8e4c3163f40a0c28e71
留白 <shr2812726002@gmail.com>
2026-06-17 20:28:07 +0800

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise')
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT || '3306'),
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '123456',
+  user: process.env.DB_USER || '自己账号',
+  password: process.env.DB_PASSWORD || '自己密码',
   database: process.env.DB_NAME || 'personality_test',
   waitForConnections: true,
   connectionLimit: 10,
